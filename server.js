cf8aba0d860c74e64d84f483f109836fa875ef6c
nathan meyer <nmeyer@nathans-MacBook-Air.local>
2019-06-28 20:02:45 -0700

    update

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ var cheerio = require("cheerio");
 
 
 // Mongoose
-if (process.env.MONGODB_URI) {
-	mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-	mongoose.connect(databaseUrl);
-};
+// if (process.env.MONGODB_URI) {
+// 	mongoose.connect(process.env.MONGODB_URI);
+// }
+// else {
+// 	mongoose.connect(databaseUrl);
+// };
 
 
 // Require all models
@@ -29,5 +29,21 @@ var port = process.env.PORT || 3000;
 
 // sets-ups
 
+// Routes
 
+// A GET route for scraping nytime.com/soccer website
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("https://www.nytimes.com/topic/subject/soccer").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cherrio.load(response.data);
+    // Now, we grab every h2 within an article tag, and do the following:
 
+    $().each(function(i, element) {
+        var result = {};
+        // save an empty result object
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
