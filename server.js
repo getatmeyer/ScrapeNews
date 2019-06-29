35592407ad42e17517aed0e67a479e3ab20be175
nathan meyer <nmeyer@D-10-19-186-186.dhcp4.washington.edu>
2019-06-29 12:18:45 -0700

    <updates>

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ var exphbs = require ("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-
-// Mongoose
-// if (process.env.MONGODB_URI) {
-// 	mongoose.connect(process.env.MONGODB_URI);
-// }
-// else {
-// 	mongoose.connect(databaseUrl);
-// };
-
-
 // Require all models
 var db = require("./models");
 
@@ -26,6 +16,27 @@ var PORT = 3000;
 // Initialize Express
 var app = express();
 var port = process.env.PORT || 3000;
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev")); // ASK DENIS about "dev"
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
+
+
+// conntact to Mongoose DB
+if (process.env.MONGODB_URI) {
+	mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+	mongoose.connect(databaseUrl);
+};
 
 // sets-ups
 
@@ -39,9 +50,33 @@ app.get("/scrape", function(req, res) {
       var $ = cherrio.load(response.data);
     // Now, we grab every h2 within an article tag, and do the following:
 
-    $().each(function(i, element) {
+    $("h2").each(function(i, element) {
         var result = {};
-        // save an empty result object
+
+        // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+      .children("a")
+      .text();
+    result.link = $(this)
+      .children("a")
+      .attr("href");
+    
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
+});
 
 // Start the server
 app.listen(PORT, function() {
